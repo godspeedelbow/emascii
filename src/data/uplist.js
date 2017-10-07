4e3844aad462391192651080d08cc8ededcf3b11
godspeedelbow <eelke@boezeman.com>
2017-10-07 22:06:03 +0200

    added tons of new emasciis

---
+++ src/data/uplist.js
@@ -0,0 +1,17 @@
+// http://upli.st/l/list-of-all-ascii-emoticons
+
+export default {
+  'friends': "(ｏ・_・)ノ”(ᴗ_ ᴗ。)",
+  'happier': '(•‿•)',
+  'kick': 'ヽ(#ﾟДﾟ)ﾉ┌┛',
+  'kick someone': 'ヽ(#ﾟДﾟ)ﾉ┌┛Σ(ノ´Д`)ノ',
+  'sad 2': '.·´¯`(>▂<)´¯`·.',
+  'dazed and confused': '(●__●)',
+  'salute': '(￣^￣)ゞ',
+  'take my money': '(╯°□°）╯$ $ $',
+  'great success': '(ง^ᗜ^)ง',
+  'party': '٩( ᐛ )و',
+  'small ghost': "(~’.')~",
+  'nobody loves me': '(._.)',
+  'exponential growth': 'ʕ•̫͡•ʔ →ʕ•̫͡•̫͡•ʔ →ʕ•̫͡•=•̫͡•ʔ →ʕ•̫͡•ʔ ʕ•̫͡•ʔ',
+};
