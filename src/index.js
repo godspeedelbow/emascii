80012dd430bd07477ba223228fbbded01a511cb6
Eelke Boezeman <eboezeman@apple.com>
2018-11-17 12:03:10 +0100

    register-service-worker.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/app";
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./register-service-worker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
